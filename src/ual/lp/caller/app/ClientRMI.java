d5db4f8699fe997b3e479fb7b145544ca2e72f9a
dcsilva <dcsilva@mail.ru>
2015-06-24 19:28:44 +0100

    Sem as merdas de Python

--- src/ual/lp/caller/app/ClientRMI.java
+++ src/ual/lp/caller/app/ClientRMI.java
@@ -14,7 +14,7 @@ import ual.lp.server.rmi.ServerInf;
  * @author Divanio Silva
  */
 public class ClientRMI {
-    public static final String HOST = "localhost";
+    public static final String HOST = "172.16.214.237";
     public static final int PORT = 1099;
     
     public static void main(String[] args) {
@@ -24,9 +24,8 @@ public class ClientRMI {
             ServerInf objRemoto = (ServerInf) registry.lookup("response");
             System.out.println(objRemoto.printMessage());
         } catch (Exception e) {
-            System.out.println("Deu merda no client.");
+//            System.out.println("Deu merda no client.");
+            System.out.println(e.getMessage());
         }
-        
-        
     }
 }
