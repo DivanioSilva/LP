6b0d65227ae7312d9468aaee399dd5b137158b07
dcsilva <dcsilva@mail.ru>
2015-06-28 17:55:12 +0100

    Implementar o EmployeeDAO.

--- src/ual/lp/server/dao/EmployeeDAO.java
+++ src/ual/lp/server/dao/EmployeeDAO.java
@@ -9,9 +9,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import javax.sql.DataSource;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import ual.lp.server.objects.Department;
 import ual.lp.server.objects.Employee;
 
 /**
@@ -19,42 +21,114 @@ import ual.lp.server.objects.Employee;
  * @author Divanio Silva, Pedro Almeida e Pedro Tomás
  */
 public class EmployeeDAO {
+
     private DataSourceTransactionManager transactionManager;
     private JdbcTemplate jdbcTemplate;
-    
-//    public int insertEmployee(Employee employee) {
-//        String sql = "insert into employees(name, department) values (?, ?);";
-////        Object[] params = new Object[]{
-////            pessoa.getName(), pessoa.getIdade()
-////        };
-//
-//        int[] types = {
-//            Types.VARCHAR, Types.VARCHAR
-//        };
-//        return jdbcTemplate.update(sql, new Object[]{employee.getName(), employee.getDepartment()}, types);
-//    }
-//    
-//    public Employee getEmployee(Employee employee){
-//        String sql = "select * from employees where name=?";
-//        return jdbcTemplate.queryForObject(sql, new Object[]{employee.getName()}, new EmployeeMapper());
-//    }
-//    
-//    private static final class EmployeeMapper implements RowMapper<Employee>{
-//
-//        @Override
-//        public Employee mapRow(ResultSet rs, int i) throws SQLException {
-//            Employee employee = new Employee(rs.getInt("idemployees"), rs.getString("name"), rs.getString("department"));
-//            return employee;
-//        }
-//        
-//    }
-    
+
+    public int insertEmployee(Employee employee) {
+        String sql = "insert into employee(name, desknumber, iddepartment) values (?, ?, ?);";
+
+        int[] types = {
+            Types.VARCHAR, Types.INTEGER, Types.INTEGER
+        };
+        return jdbcTemplate.update(sql, new Object[]{employee.getName(), employee.getDeskNumber(), employee.getDepartment().getId()}, types);
+    }
+
+    public boolean verifyEmployee(Employee employee) {
+
+        String sql = null;
+
+        sql = "select * from department where department = ? and abbreviation = ?;";
+
+        try {
+            jdbcTemplate.queryForObject(sql, new Object[]{employee.getDepartment().getName(), employee.getDepartment().getAbbreviation()}, new DepartmentMapper());
+
+        } catch (Exception e) {
+            //não existe o departemento, precisamos adiciona-lo na DB
+            sql = "insert into department(department, abbreviation) values(?, ?);";
+
+            int[] types = {
+                Types.VARCHAR, Types.VARCHAR
+            };
+
+            jdbcTemplate.update(sql, new Object[]{employee.getDepartment().getName(), employee.getDepartment().getAbbreviation()}, types);
+        }
+
+        //ver se ele já esta na DB.
+        sql = "select * from employee where employee.name=?;";
+
+        try {
+            //se passar, é pq existe o employee na db.
+            jdbcTemplate.queryForObject(sql, new Object[]{employee.getName()}, new simpleEmployeeMapper());
+
+        } catch (Exception e) {
+            
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //ver se ele já esta na DB.
+        //se não, inseri-lo
+        sql = "select * from employee\n"
+                + "join department on employee.iddepartment=department.iddepartment\n"
+                + "where employee.name=?;";
+
+        try {
+            jdbcTemplate.queryForObject(sql, new Object[]{employee.getName()}, new EmployeeMapper());
+            return true;
+
+        } catch (EmptyResultDataAccessException e) {
+            sql = "insert into employee(name, desknumber, iddepartment) values (?, ?, ?);";
+
+            int[] types = {
+                Types.VARCHAR, Types.INTEGER, Types.INTEGER
+            };
+
+            jdbcTemplate.update(sql, new Object[]{employee.getName(), employee.getDeskNumber(), employee.getDepartment().getId()}, types);
+
+            return false;
+        }
+    }
+
+    private static final class EmployeeMapper implements RowMapper<Employee> {
+
+        @Override
+        public Employee mapRow(ResultSet rs, int i) throws SQLException {
+            Department department = new Department(rs.getInt("iddepartment"), rs.getString("department"), rs.getString("abbreviation"));
+            Employee employee = new Employee(rs.getInt("idemployee"), rs.getString("name"), rs.getInt("desknumber"), department);
+            return employee;
+        }
+    }
+
+    private static final class simpleEmployeeMapper implements RowMapper<Employee> {
+
+        @Override
+        public Employee mapRow(ResultSet rs, int i) throws SQLException {
+            Employee employee = new Employee(rs.getInt("idemployee"), rs.getString("name"), rs.getInt("desknumber"));
+            return employee;
+        }
+    }
+
+    private static final class DepartmentMapper implements RowMapper<Department> {
+
+        @Override
+        public Department mapRow(ResultSet rs, int i) throws SQLException {
+            Department department = new Department(rs.getInt("iddepartment"), rs.getString("department"), rs.getString("abbreviation"));
+            return department;
+        }
+    }
+
     public void setTransactionManager(DataSourceTransactionManager transactionManager) {
         this.transactionManager = transactionManager;
         DataSource dataSource = transactionManager.getDataSource();
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
-    
-    
 
 }
