3e39793c0283a7399466d0380d22a621e23aca0e
dcsilva <dcsilva@mail.ru>
2015-06-28 15:51:58 +0100

    Altereações no caller.

--- src/ual/lp/caller/utils/Main.java
+++ src/ual/lp/caller/utils/Main.java
@@ -5,6 +5,8 @@
  */
 package ual.lp.caller.utils;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import ual.lp.server.objects.Employee;
 
 /**
@@ -12,11 +14,15 @@ import ual.lp.server.objects.Employee;
  * @author Divanio Silva
  */
 public class Main {
-    
-    public static void main(String[] args) {
-        ConfigReader conf = new ConfigReader();
+
+    public static void main(String[] args) throws UnknownHostException {
+        Config conf = new Config();
         Employee emp = new Employee();
-        emp=conf.configEmployee();
-        System.out.println(emp);
+        emp = conf.getEmployee();
+        conf.getEmployee();
+        System.out.println(emp.getName()+", "+emp.getDepartment().getAbbreviation()+", "+emp.getDeskNumber()+", "+emp.getDepartment().getName());
+        System.out.println("MyIP: "+conf.getMyIP());
+        System.out.println(Inet4Address.getLocalHost().getHostAddress());
+        System.out.println("ServerIP: "+conf.getServerIP());
     }
 }
