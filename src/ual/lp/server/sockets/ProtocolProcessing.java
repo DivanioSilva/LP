21a91424fcc7da2450232070b98c72ccb8d885c3
Pedro <pedro tomas>
2015-07-10 18:37:28 +0100

    Criei as classes de teste v0.5

--- src/ual/lp/server/sockets/ProtocolProcessing.java
+++ src/ual/lp/server/sockets/ProtocolProcessing.java
@@ -6,54 +6,64 @@
 package ual.lp.server.sockets;
 
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
 import ual.lp.server.mgr.Manager;
+import ual.lp.server.objects.Department;
 
 /**
  *
  * @author Pedro
  */
 public class ProtocolProcessing {
-    private String in,out;
+
+    private String in, out;
     private PrintWriter sktOut;
     private Manager manager;
-    
-    
-    
-    
-    public ProtocolProcessing(PrintWriter sktOut){
-        this.sktOut=sktOut;
+
+    public ProtocolProcessing(PrintWriter sktOut) {
+        this.sktOut = sktOut;
     }
-    
-    
-     
-    public ProtocolProcessing(PrintWriter sktOut,Manager manager){
-        this.sktOut=sktOut;
+
+    public ProtocolProcessing(PrintWriter sktOut, Manager manager) {
+        this.sktOut = sktOut;
         this.manager = manager;
     }
-    
-    
-    void inMessage (String in){
-        this.in=in;
-        
+
+    void inMessage(String in) {
+        this.in = in;
+        List<String> departamentos;
+
         switch (in) {
             case "Hello from Asura CPRN!":
                 System.out.println("Ele disse olá, oh meu deus o que vou fazer ?");
-                
-                //corrigir
-                new Manager(false).autoCreateTicket("Secretaria");
+                departamentos = new Manager(false).getDepartmentsString();
+                String list = departamentos.toString();
+                String csv = list.substring(1, list.length() - 1).replace(", ", ",");
+                System.out.println("#######################");
+                System.out.println("#######################");
+                System.out.println(csv);
+                System.out.println("#######################");
+                System.out.println(departamentos.toString());
+                System.out.println("#######################");
 
-                sktOut.println("Recebido: "+"Ele disse olá, oh meu deus o que vou fazer ?");
-                
+                sktOut.println("Recebido: " + "Ele disse olá, oh meu deus o que vou fazer ?");
+ 
+                break;
+            case "Request":
+
+                //corrigir
+
                 // Pckg ual.lp.server.mgr/Manager.java
                 // Metodo:autocreateticket (departamento) devolve numero
-                // 
-            case "Request":
                 
-            break;
-                
-                
-                
+                sktOut.println("---------------- WTF ----------------");
+                new Manager(false).autoCreateTicket("Secretaria");
+                new Manager(false).autoCreateTicket("Tesouraria");
+
+                break;
+
+        }
+
     }
-    
-}
 }
