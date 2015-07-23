81b1b6eab5b4ce9fb317ef0cdfb0eac504a639c3
dcsilva <dcsilva@mail.ru>
2015-07-23 18:26:48 +0100

    Service thread

--- src/ual/lp/server/mgr/ServiceThread.java
+++ src/ual/lp/server/mgr/ServiceThread.java
@@ -5,32 +5,41 @@
  */
 package ual.lp.server.mgr;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author Divanio Silva
  */
-public class ServiceThread implements Runnable{
+public class ServiceThread implements Runnable {
 
+    static final Logger log = Logger.getLogger("debugLogger");
     private Manager manager;
+    private boolean running;
 
     public ServiceThread(Manager manager) {
         this.manager = manager;
+        log.debug("a thread de serviço do server arrancou.");
+        this.running = true;
     }
 
     public ServiceThread() {
     }
-    
+
     @Override
     public void run() {
-        
-        try {
-            for (int i = 0; i < manager.getDepartments().size(); i++) {    
-                manager.employeesCallback(manager.getDepartments().get(i));
+        while (running) {
+            try {
+                log.debug("Tenho: " + manager.getDepartments().size());
+                for (int i = 0; i < manager.getDepartments().size(); i++) {
+                    manager.employeesCallback(manager.getDepartments().get(i));
+                    log.debug(manager.getDepartments().get(i).getName());
+                }
+
+                Thread.sleep(1000);
+            } catch (Exception e) {
             }
-            
-            Thread.sleep(1000);
-        } catch (Exception e) {
-        }        
+        }
     }
-    
+
 }
